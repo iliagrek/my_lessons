640bf06ef7ce852993ddf40c47194001ed9fa32d
iliagrek <knos266025@mail.ru>
2024-12-17 10:08:15 +0300

    Add files via upload

---
+++ modul_5_4.py
@@ -0,0 +1,24 @@
+class House():
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Медовая долина', 10)
+print(House.houses_history)
+h2 = House('ЖК У озера', 20)
+print(House.houses_history)
+h3 = House('ЖК Березовая роща', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
