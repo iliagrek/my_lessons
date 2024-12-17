8eac56f996356236ec712ba0f18e48c3e2b00bbf
iliagrek <knos266025@mail.ru>
2024-12-17 09:11:27 +0300

    Add files via upload

---
+++ modul_5_3.py
@@ -0,0 +1,66 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует!')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        new_number = self.number_of_floors
+        if isinstance(value, House):
+            new_number += value.number_of_floors
+        elif isinstance(value, int):
+            new_number += value
+            return House(self.name, new_number)
+
+    __radd__ = __add__
+    __iadd__ = __add__
+
+
+h1 = House('ЖК Медовая долина', 10)
+h2 = House('ЖК У озера', 20)
+
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
